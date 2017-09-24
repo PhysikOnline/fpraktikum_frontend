f4a202ffbe7981460aae4c7610a0e26575b21fc9
Lars Gröber <larsgroeber7@gmail.com>
2017-09-24 11:51:16 +0200

    added moment.js to format dates

--- frontend/src/app/not-intime/not-intime.component.ts
+++ frontend/src/app/not-intime/not-intime.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { RegistrationService } from '../../services/registration.service';
 import { User } from '../../models/user';
 import { InTime } from '../../models/in-time';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-not-intime',
@@ -12,7 +13,7 @@ export class NotIntimeComponent implements OnInit {
   @Input() set tooLate(tooLate: InTime) {
     this.isTooLate = tooLate === InTime.tooLate;
     const date = this.isTooLate ? this.registrationService.date.end : this.registrationService.date.start;
-    this.date = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
+    this.date = moment(date).format('DD.MM.YYYY HH:mm');
   };
   isTooLate: boolean;
   date: string;
