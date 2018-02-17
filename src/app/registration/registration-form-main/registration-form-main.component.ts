6eea607e5392182ec7204c64466c2df94f4ac71f
Lars Gröber <larsgroeber7@gmail.com>
2018-02-18 00:07:27 +0100

    added institute update to main form

--- src/app/registration/registration-form-main/registration-form-main.component.ts
+++ src/app/registration/registration-form-main/registration-form-main.component.ts
@@ -15,7 +15,7 @@ import { debounceTime, filter } from 'rxjs/operators';
 
 import * as selectors from '../store/selectors';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { CheckPartner } from '../store/index';
+import { CheckPartner, UpdateSelectedInstitutes } from '../store/index';
 import { RemovePartner } from '../store/actions/partner.action';
 import { UpdateNotes } from '../store/actions/user.action';
 import { ChosenPartner } from '../../models/chosen-partner';
@@ -56,10 +56,12 @@ export class RegistrationFormMainComponent implements OnInit, OnDestroy {
   readonly notesForm: FormGroup;
 
   readonly partnerInput = new Subject();
-  readonly institutesSelected: ReplaySubject<Institute> = new ReplaySubject(1);
+  readonly institutesSelect: ReplaySubject<Institute> = new ReplaySubject(1);
   readonly notesInput: ReplaySubject<void> = new ReplaySubject(1);
 
   readonly noPartner = new BehaviorSubject(false);
+
+  institutes: Institute[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -88,27 +90,20 @@ export class RegistrationFormMainComponent implements OnInit, OnDestroy {
       }
     });
 
-    this.sink = this.institutesSelected.subscribe(institute => {});
+    this.sink = Observable.combineLatest(
+      this.institutesSelect,
+      this.chooseOnlyOneInstitute
+    ).subscribe(([institute, oneInstitute]) => {
+      this.updateInstitutes(institute, oneInstitute);
+    });
 
     this.sink = this.partnerInput
       .pipe(filter(() => this.partnerForm.valid), debounceTime(500))
-      .subscribe(() => {
-        this.checkPartner();
-      });
-    this.sink = this.noPartner.subscribe(res => {
-      if (res) {
-        this.partnerStore.dispatch(new RemovePartner());
-        this.partnerForm.reset();
-        this.partnerForm.disable();
-      } else {
-        this.partnerForm.enable();
-      }
-    });
-    this.sink = this.notesInput.pipe(debounceTime(500)).subscribe(() => {
-      const notes = this.notesForm.get('notes').value;
-      this.userStore.dispatch(new UpdateNotes(notes));
-    });
-    this.sink = this.institutesSelected.subscribe();
+      .subscribe(this.checkPartner.bind(this));
+    this.sink = this.noPartner.subscribe(this.onNoPartner.bind(this));
+    this.sink = this.notesInput
+      .pipe(debounceTime(500))
+      .subscribe(this.onNotesUpdate.bind(this));
   }
 
   checkPartner() {
@@ -118,6 +113,21 @@ export class RegistrationFormMainComponent implements OnInit, OnDestroy {
     this.partnerStore.dispatch(new CheckPartner({ number, name }));
   }
 
+  private onNoPartner(res: boolean): void {
+    if (res) {
+      this.partnerStore.dispatch(new RemovePartner());
+      this.partnerForm.reset();
+      this.partnerForm.disable();
+    } else {
+      this.partnerForm.enable();
+    }
+  }
+
+  private onNotesUpdate(): void {
+    const notes = this.notesForm.get('notes').value;
+    this.userStore.dispatch(new UpdateNotes(notes));
+  }
+
   startNextStep() {
     if (this.institutesForm.invalid) {
       return;
@@ -125,13 +135,35 @@ export class RegistrationFormMainComponent implements OnInit, OnDestroy {
     this.metaStore.dispatch(new UpdateRegistrationStep(REGISTRATION_STEP.END));
   }
 
+  private updateInstitutes(institute: Institute, oneInstitute: boolean): void {
+    if (oneInstitute) {
+      return this.metaStore.dispatch(new UpdateSelectedInstitutes([institute]));
+    }
+    const index = this.institutes.findIndex(
+      i => i.semesterHalf === institute.semesterHalf
+    );
+    if (index > -1) {
+      this.institutes[index] = institute;
+    } else {
+      this.institutes.push(institute);
+    }
+    return this.metaStore.dispatch(
+      new UpdateSelectedInstitutes(this.institutes)
+    );
+  }
+
   shouldOptionBeDisabled(instituteName, semesterHalf) {
-    const otherHalf = (semesterHalf + 1) % 2 + 1;
+    const otherHalf = semesterHalf % 2 + 1;
     return this.selectedInstitutes.map(institutes =>
       institutes.find(
         i => i.name === instituteName && i.semesterHalf === otherHalf
       )
     );
+  }
+
+  resetInstitutes() {
+    this.institutesForm.reset();
+    this.metaStore.dispatch(new UpdateSelectedInstitutes([]));
   }
 
   ngOnInit() {}
